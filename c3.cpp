6faa0bd1a55a921b5c507b569deacd56cb499105
Shivam kumar Mishra <sivm.skm@outlook.com>
2021-05-12 19:55:14 +0530

    modified it and still is not working

--- c3.cpp
+++ c3.cpp
@@ -3,26 +3,20 @@ using namespace  std;
 
 int taking_input(int arr[],int size_arr)
 {
-    for(int num =0;num<size_arr;num++)
+    for(int i=0;i<size_arr;i++)
     {
-        cout<<arr[num]<<endl;
-    
+        //tacking input
+        cin>>arr[i];
+    }
+    //printing array elemet
+    for(int i=0;i<size_arr;i++)
+    {
+        cout<<arr[i]<<endl;
     }
     return 0;
 }
 int main()
 {
-    int size;
-    int array[size];
-    cout<<"enter the total number of array \n";
-    cin>>size;
-    cout<<"enter the elements of array\n";
-    for(int i =0;i<size;i++)
-    {
-        cin>>array[i];
-        
-    }
-    taking_input( array,size);
-
-
+    int size, arr[size];
+    taking_input(arr,size);
 }
