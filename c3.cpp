ae922229c3760c06ab23701be993f2519bc9286b
Shivam kumar Mishra <sivm.skm@outlook.com>
2021-05-12 20:25:56 +0530

    third commit successfully

--- c3.cpp
+++ c3.cpp
@@ -1,14 +1,8 @@
 #include<iostream>
-using namespace  std;
+using namespace std;
 
-int taking_input(int arr[],int size_arr)
+int tacking_input(int arr[],int size_arr)
 {
-    for(int i=0;i<size_arr;i++)
-    {
-        //tacking input
-        cin>>arr[i];
-    }
-    //printing array elemet
     for(int i=0;i<size_arr;i++)
     {
         cout<<arr[i]<<endl;
@@ -17,6 +11,13 @@ int taking_input(int arr[],int size_arr)
 }
 int main()
 {
-    int size, arr[size];
-    taking_input(arr,size);
+    int size ,arr[size];
+    cout<<"enter the size of total array\n";
+    cin>>size;
+    cout<<"enter the element\n";
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    tacking_input(arr,size);
 }
