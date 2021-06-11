8f3fece144dd37f23e1aacda38de0502c7d7f70c
Shivam kumar Mishra <sivm.skm@outlook.com>
2021-06-11 10:58:03 +0530

    size of array return only the first array size

--- gradingsystem.cpp
+++ gradingsystem.cpp
@@ -2,20 +2,11 @@
 using namespace std;
 int main()
 {
-    int array1[9] = {1,2,3,4,5,6,7,8,9};
-    int array2[9] = {9,8,7,6,5,4,3,2,1};
-    int i,sum,tot[9];
+    int arr1[3][3] = {1,2,3,4,5,6,7,8,9};
+    int arr2[3][3]= {1,2,3,4,5,6,7,8,9};
+    int i,j,k,l ;
+    static int tot;
+    cout<<size_t(arr1) <<endl;
 
-    for(i=0;i<9;i++)
-    {
-        for(int j=0;j<9;j++)
-        {
-            if(i == j)
-            {
-                cout<<array1[i] + array2[j] <<endl;
-                cout<<"___\n\n";
-            }
-        }
-    }
-
+    cout<<sizeof(arr1)/sizeof(arr1[1])<<endl;
 }
